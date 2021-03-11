40edbc8e27737c5f1e0f4526f089b90108074d82
Augusto <augustocristian@uniovi.es>
2021-03-11 22:24:20 +0100

    PID tunning for the BED at 90 grades celsius

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -515,7 +515,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-//#define PIDTEMPBED
+#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -533,9 +533,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 20.49
-  #define DEFAULT_bedKi 2.78
-  #define DEFAULT_bedKd 37.76
+  #define DEFAULT_bedKp 161.66
+  #define DEFAULT_bedKi 31.83
+  #define DEFAULT_bedKd 547.37
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
